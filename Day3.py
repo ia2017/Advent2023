ede4e5c4ff5f91ba636787dfd30dc5e7fdd3aef7
Imran bin Ahmad Azhar <imranahmazh@gmail.com>
2023-12-06 13:23:25 +0000

    IA: finished Day3 P2

--- Day3.py
+++ Day3.py
@@ -1,12 +1,13 @@
 with open("inputs/Day3", newline="\n") as f:
     lines = [line.rstrip() for line in f]
 
-print(lines)
-
 part_sum = 0
 symbol_bool = 0
 total_sum = 0
 char = ""
+gear_index = {}
+gear_ratio = 0
+pairs = []
 
 for i in range(len(lines)):
     line = lines[i]
@@ -25,12 +26,26 @@ for i in range(len(lines)):
             # LEFT
             if line[j - 1 - number_index] != "." and not line[j - 1 - number_index].isdigit():
                 symbol_bool += 1
+
+                if line[j - 1 - number_index] == "*" and f"{i}, {j - 1 - number_index}" not in gear_index.keys():
+                    gear_index[f"{i}, {j - 1 - number_index}"] = current_number
+                elif f"{i}, {j - 1 - number_index}" in gear_index.keys():
+                    gear_ratio += current_number * gear_index[f"{i}, {j - 1 - number_index}"]
+                    pairs.append([gear_index[f"{i}, {j - 1 - number_index}"], current_number])
+                    del gear_index[f"{i}, {j - 1 - number_index}"]
+
                 # symbols.append(line[j - 1 - number_index])
 
             # RIGHT
             if char != "." and not char.isdigit():
                 symbol_bool += 1
                 # symbols.append(char)
+                if char == "*" and f"{i}, {j}" not in gear_index.keys():
+                    gear_index[f"{i}, {j}"] = current_number
+                elif f"{i}, {j}" in gear_index.keys():
+                    gear_ratio += current_number * gear_index[f"{i}, {j}"]
+                    pairs.append([current_number, gear_index[f"{i}, {j}"]])
+                    del gear_index[f"{i}, {j}"]
 
             # TOP & BOTTOM
             for k in range(j - 1 - number_index, j + 1, 1):
@@ -39,17 +54,33 @@ for i in range(len(lines)):
                     if lines[i - 1][k] != "." and not lines[i - 1][k].isdigit():
                         symbol_bool += 1
                         # symbols.append(lines[i - 1][k])
+                        if lines[i - 1][k] == "*" and f"{i - 1}, {k}" not in gear_index.keys():
+                            gear_index[f"{i - 1}, {k}"] = current_number
+                        elif f"{i - 1}, {k}" in gear_index.keys():
+                            gear_ratio += current_number * gear_index[f"{i - 1}, {k}"]
+                            pairs.append([gear_index[f"{i - 1}, {k}"], current_number])
+
+                            del gear_index[f"{i - 1}, {k}"]
 
                 # Bottom
                 if i < len(lines) - 1:
                     if lines[i + 1][k] != "." and not lines[i + 1][k].isdigit():
                         symbol_bool += 1
                         # symbols.append(lines[i + 1][k])
+                        if lines[i + 1][k] == "*" and f"{i + 1}, {k}" not in gear_index.keys():
+                            gear_index[f"{i + 1}, {k}"] = current_number
+                        elif f"{i + 1}, {k}" in gear_index.keys():
+                            gear_ratio += current_number * gear_index[f"{i + 1}, {k}"]
+                            pairs.append([gear_index[f"{i + 1}, {k}"], current_number])
 
-            print(f"current_number : {current_number}, bool : {symbol_bool}, "
-                  # f"symbols: {symbols}"
-                  )
+                            del gear_index[f"{i + 1}, {k}"]
+
+            # print(f"current_number : {current_number}, bool : {symbol_bool}, "
+            #       # f"symbols: {symbols}"
+            #       )
             # print(number_index)
+
+            # print(gear_index)
 
             if symbol_bool > 0:
                 part_sum += current_number
@@ -59,7 +90,6 @@ for i in range(len(lines)):
             number_index = 0
             current_number = ""
             symbols = []
-
 
         else:
             pass
@@ -71,7 +101,12 @@ for i in range(len(lines)):
         if line[j - number_index] != "." and not line[j - number_index].isdigit():
             symbol_bool += 1
             # symbols.append(line[j - 1 - number_index])
-
+            if line[j - number_index] == "*" and f"{i}, {j - number_index}" not in gear_index.keys():
+                gear_index[f"{i}, {j - number_index}"] = current_number
+            elif f"{i}, {j - number_index}" in gear_index.keys():
+                gear_ratio += current_number * gear_index[f"{i}, {j - number_index}"]
+                pairs.append([gear_index[f"{i}, {j - number_index}"], current_number])
+                del gear_index[f"{i}, {j - number_index}"]
 
         # TOP & BOTTOM
         for k in range(j - number_index, j + 1, 1):
@@ -80,6 +115,13 @@ for i in range(len(lines)):
                 if lines[i - 1][k] != "." and not lines[i - 1][k].isdigit():
                     symbol_bool += 1
                     # symbols.append(lines[i - 1][k])
+                    if lines[i - 1][k] == "*" and f"{i - 1}, {k}" not in gear_index.keys():
+                        gear_index[f"{i - 1}, {k}"] = current_number
+                    elif f"{i - 1}, {k}" in gear_index.keys():
+                        gear_ratio += current_number * gear_index[f"{i - 1}, {k}"]
+                        pairs.append([gear_index[f"{i - 1}, {k}"], current_number])
+
+                        del gear_index[f"{i - 1}, {k}"]
 
             # Bottom
             if i < len(lines) - 1:
@@ -87,9 +129,17 @@ for i in range(len(lines)):
                     symbol_bool += 1
                     # symbols.append(lines[i + 1][k])
 
-        print(f"current_number : {current_number}, bool : {symbol_bool}, "
-              # f"symbols: {symbols}"
-              )
+                    if lines[i + 1][k] == "*" and f"{i + 1}, {k}" not in gear_index.keys():
+                        gear_index[f"{i + 1}, {k}"] = current_number
+                    elif f"{i + 1}, {k}" in gear_index.keys():
+                        gear_ratio += current_number * gear_index[f"{i + 1}, {k}"]
+                        pairs.append([gear_index[f"{i + 1}, {k}"], current_number])
+
+                        del gear_index[f"{i + 1}, {k}"]
+
+        # print(f"current_number : {current_number}, bool : {symbol_bool}, "
+        #       # f"symbols: {symbols}"
+        #       )
         # print(number_index)
 
         if symbol_bool > 0:
@@ -101,104 +151,6 @@ for i in range(len(lines)):
         current_number = ""
         symbols = []
 
-
-
-        # if not char.isdigit() and char != ".":
-        #     print(f"i, j : {i}, {j}")
-        #
-        #     # ONLY MIDDLE
-        #     if j != 0 or j != len(line) - 1:
-        #         # Checks if top
-        #         if lines[i][j - 1].isdigit() or lines[i][j].isdigit() or lines[i][j + 1].isdigit():
-        #             # Get left
-        #             number_left = ""
-        #             for k in range(j - 1, 0, -1):
-        #                 if lines[i][k] == ".":
-        #                     break
-        #                 number_left = str(lines[i][k]) + number_left
-        #
-        #             # Get right
-        #             number_right = ""
-        #             for k in range(j + 1, len(lines[i]) - 1, 1):
-        #                 if lines[i][k] == ".":
-        #                     break
-        #                 number_right += str(lines[i][k])
-        #
-        #             # Get middle
-        #             if lines[i][j] == ".":
-        #                 number_middle = " "
-        #             else:
-        #                 number_middle = lines[i][j]
-        #
-        #             number_top = number_left + number_middle + number_right
-        #
-        #             number_top = number_top.split(" ")
-        #
-        #             for num in number_top:
-        #                 if num.isdigit():
-        #                     part_sum += int(num)
-        #
-        #         # Checks if bottom
-        #         if lines[i + 2][j - 1].isdigit() or lines[i + 2][j].isdigit() or lines[i + 2][j + 1].isdigit():
-        #             # Get left
-        #             number_left = ""
-        #             for k in range(j - 1, 0, -1):
-        #                 if lines[i + 2][k] == ".":
-        #                     break
-        #                 number_left = str(lines[i + 2][k]) + number_left
-        #
-        #             # Get right
-        #             number_right = ""
-        #             for k in range(j + 1, len(lines[i + 2]) - 1, 1):
-        #                 if lines[i + 2][k] == ".":
-        #                     break
-        #                 number_right += str(lines[i + 2][k])
-        #
-        #             # Get middle
-        #             if lines[i + 2][j] == ".":
-        #                 number_middle = " "
-        #             else:
-        #                 number_middle = lines[i + 2][j]
-        #
-        #             number_top = number_left + number_middle + number_right
-        #
-        #             number_top = number_top.split(" ")
-        #
-        #             for num in number_top:
-        #                 if num.isdigit():
-        #                     part_sum += int(num)
-        #
-        #         # LEFT
-        #         if line[j - 1].isdigit():
-        #             number = ""
-        #             for k in range(j - 1, 0, -1):
-        #                 if line[k] == ".":
-        #                     break
-        #                 number = str(line[k]) + number
-        #
-        #             number = int(number)
-        #             part_sum += number
-        #
-        #         # RIGHT
-        #         if line[j + 1].isdigit():
-        #             number = ""
-        #             for k in range(j + 1, len(line) - 1, 1):
-        #                 if line[k] == ".":
-        #                     break
-        #                 number += line[k]
-        #
-        #             number = int(number)
-        #             part_sum += number
-        #
-        #
-        #         # if lines[i + 2][j - 1].isdigit():
-        #         #     print("bottom left")
-        #         # if lines[i + 2][j].isdigit():
-        #         #     print("bottom")
-        #         # if lines[i + 2][j + 1].isdigit():
-        #         #     print("bottom right")
-        #
-        # # Checking if number
-
+print(pairs)
 print(part_sum)
-print(total_sum)
+print(gear_ratio)
