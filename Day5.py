bf57259d7b80073fd400966e8ca66379ac6d441a
Imran bin Ahmad Azhar <imranahmazh@gmail.com>
2023-12-06 17:39:50 +0000

    IA: Day 5 progressing

--- Day5.py
+++ Day5.py
@@ -1,4 +1,21 @@
-with open("inputs/Day4", newline="\n") as f:
+with open("inputs/Day5", newline="\n") as f:
     lines = [line.rstrip() for line in f]
 
+seed_dict = {}
+seed_title = 
+
 print(lines)
+
+for i, line in enumerate(lines):
+    if i == 0:
+        seed_line = line.split(":")[1].split(" ")
+        seed_line.remove('')
+        seeds = [int(x) for x in seed_line]
+    else:
+        if ":" in line:
+            seed_title = line
+            seed_dict[seed_title] = []
+        else:
+
+
+print(seeds)
